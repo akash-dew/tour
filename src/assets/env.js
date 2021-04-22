ae87af31ee7cc28d89811fa1fd44d431e820bd73
saivarudu <saisunder12345@gmail.com>
2021-04-22 15:07:49 +0530

    changes

--- src/assets/env.js
+++
@@ -1,8 +0,0 @@
-(function(window) {
-    window["env"] = window["env"] || {};
-  
-    // Environment variables
-    window["env"]["baseUrl"] = "https://afs-dev.testedgeonline.com";
-    window["env"]["loginType"] = 0;
-    window["env"]["showMarkAsComplete"] = 1;
-  })(this);
--- src/assets/env.template.js
+++
@@ -1,8 +0,0 @@
-(function(window) {
-    window.env = window.env || {};
-  
-    // Environment variables
-    window["env"]["baseUrl"] = "${API_URL}";
-    window["env"]["loginType"] = "${LOGIN_TYPE}";
-    window["env"]["showMarkAsComplete"] = "${MARK_AS_COMPLETE}";
-  })(this);
